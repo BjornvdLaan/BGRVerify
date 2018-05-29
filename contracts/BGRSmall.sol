23bf7c179abf322f30ee0d8695255f25da5e1d71
Bjorn van der Laan <bjorn.vdlaan@gmail.com>
2018-05-29 20:57:19 +0200

    preparing transition uint to int

--- contracts/BGRSmall.sol
+++ contracts/BGRSmall.sol
@@ -3,71 +3,43 @@ pragma experimental ABIEncoderV2;
 
 contract BGRSmall {
 
-    bytes32 x = bytes32(18115390633149407653339833018649393973660880445469525020077358032081985937997);
-    bytes32 h = bytes32(11559775926174368905745592433081596904384683309495480316690029641247127684734);
+    bytes32 x = bytes32(42824850096903168933980549444850820531267307277747638740663904061453936159818);
+    bytes32 h = bytes32(5501940602124613446706973589804112402000519963019969627929571506777536396930);
 
-    bool[] b = [false, false, false];
-    bytes2[] r = [bytes2(6566), bytes2(31966), bytes2(60634)];
+    bool[] b = [false];
+    bytes2[] r = [bytes2(60884)];
 
     string[] messages = [
-    "MESSAGE 0",
-    "MESSAGE 1",
-    "MESSAGE 2"
+    "MESSAGE 0"
     ];
 
     //Simulation of PKI
     uint256 e = 65537;
     uint256[] modulus = [
-    97885221880545131614956327669353796100886093042909598927834387463561749905353,
-    91542019175266321384222930621953011415273958814303191515601836249299334964037,
-    96346107315202996585852262992425589636144460118263479845834058492693067329513
+    72929305810002043507619804809205202485847149948104996985858670357851653292683
     ];
 
-    function test() returns(bool) {
-        /*
-       x0 48067657424336353606332271955203152262108042684260758989777771218257675402919
-       h0 110698550962008875660816179094143079823491013343052579972863588172519883248496
-       b: [false true]
-       pk0: [{100559639697891460902764141020356907258428814093396659529555505846242710753001 65537}
-       pk1: {75514132473334443068127286379962420780446675478900207212147158057076204097627 65537}]
-       sigx 5478324901913405590120700960084756849033925727756286198862916110070595692811
-       sigh 31908922008483892227221543185441079020097797844702142216235016446839414122921
-       r0  31713
-       r1 16181
-       */
+    function test() returns (int256) {
+        bytes32 x = bytes32(487670593740862315430319396702514703259307367722539249477064700793573449442);
+        bytes32 hash = keccak256(x);
+        return int256(hash) - int256(57896044618658097711785492504343953926634992332820282019728792003956564819968);
+        //return hash;//uint256(hash);
 
-        bytes32 x_prev = bytes32(5478324901913405590120700960084756849033925727756286198862916110070595692811); //sig.x
-        bytes32 h_prev = bytes32(31908922008483892227221543185441079020097797844702142216235016446839414122921); //sig.h
-
-        bytes32 g;
-        bytes32 eta;
-        bytes32 y;
-        bytes32 X;
-
-        //Line 2
-        X = split_inverse(true, x_prev);
-        y = pi(x_prev, 75514132473334443068127286379962420780446675478900207212147158057076204097627);
-
-        //Line 3 G
-        g = GHash(h_prev);
-
-        //Line 4
-        x_prev = g ^ y;
-
-        //Line 5 H
-        eta = HHash(75514132473334443068127286379962420780446675478900207212147158057076204097627, messages[1], 16181, x_prev);
-
-        //Line 6
-        h_prev = h_prev ^ eta;
-
-        //Now should be x1 h1
-        bytes32 x_0 = bytes32(48067657424336353606332271955203152262108042684260758989777771218257675402919);
-        bytes32 h_0 = bytes32(110698550962008875660816179094143079823491013343052579972863588172519883248496);
-        return h_prev == h_0 && x_prev == x_0;
     }
+    //Solidity uint is circular and Go big int can go negative.
+
+    /*
+    x_i: [94 174 1 149 209 38 241 132 198 77 127 199 228 178 157 116 233 91 190 240 115 235 37 155 83 232 192 237 185 120 236 74]
+    h_i: [12 41 252 239 56 158 231 221 173 143 219 185 196 46 254 2 35 79 155 162 1 86 12 66 106 124 214 101 242 140 254 130]
+    b: [false]
+    pk: [{72929305810002043507619804809205202485847149948104996985858670357851653292683 65537}]
+    x 42824850096903168933980549444850820531267307277747638740663904061453936159818
+    h 5501940602124613446706973589804112402000519963019969627929571506777536396930
+    r  60884
+    */
 
     //function verify(string[] messages, bytes32 x, bytes32 h, bytes2[] r, bool[] b) returns (bool) {
-    function verify() returns (bool) {
+    function verify() returns (bytes32) {
         bytes32 x_prev = x;
         bytes32 h_prev = h;
 
@@ -95,26 +67,25 @@ contract BGRSmall {
         }
 
         //Line 7
-        //bytes32 h_hash = HHash(modulus[0], messages[0], r[0], bytes32(0)); //this is tested
         bytes32 h_hash = HHashBase(modulus[0], messages[0], r[0]);
-        bytes32 g_hash = GHash(h_prev); //this is tested
+        bytes32 g_hash = GHash(h_prev);
 
-        bytes32 pig = pi(split_inverse(b[0], x_prev), modulus[0]); //this is tested
+        bytes32 pig = pi(x_prev, modulus[0]); //split_inverse(b[0], x_prev)
 
-        return g_hash == pig && h_hash == h_prev;
+        return g_hash;
+        //return g_hash == pig && h_hash == h_prev;
     }
 
-    function GHash(bytes32 x) pure returns (bytes32) {
+    function GHash(bytes32 x) internal pure returns (bytes32) {
         bytes32 hash = keccak256(x);
-        //hash[0] = 0x00;
-        return hash;
+        return bytes32(uint256(hash) - uint256(57896044618658097711785492504343953926634992332820282019728792003956564819968));
     }
 
-    function HHash(uint256 pk, string m, bytes2 r, bytes32 x) pure returns (bytes32) {
+    function HHash(uint256 pk, string m, bytes2 r, bytes32 x) internal pure returns (bytes32) {
         return keccak256(pk, m, r, x);
     }
 
-    function HHashBase(uint256 pk, string m, bytes2 r) pure returns (bytes32) {
+    function HHashBase(uint256 pk, string m, bytes2 r) internal pure returns (bytes32) {
         // this seems to work if Go uses x=nil
         return keccak256(pk, m, r);
     }
@@ -123,7 +94,7 @@ contract BGRSmall {
         return bytes32(modExp(uint256(val), e, mod));
     }
 
-    function modExp(uint256 base, uint256 exp, uint256 mod) internal returns (uint256 result)  {
+    function modExp(uint256 base, uint256 exp, uint256 mod) internal pure returns (uint256 result)  {
         result = 1;
         for (uint count = 1; count <= exp; count *= 2) {
             if (exp & count != 0)
@@ -132,18 +103,7 @@ contract BGRSmall {
         }
     }
 
-    function stringToBytes32(string memory source) returns (bytes32 result) {
-        bytes memory tempEmptyStringTest = bytes(source);
-        if (tempEmptyStringTest.length == 0) {
-            return 0x0;
-        }
-
-        assembly {
-            result := mload(add(source, 32))
-        }
-    }
-
-    function split_inverse(bool b, bytes32 x) internal returns (bytes32) {
+    function split_inverse(bool b, bytes32 x) internal pure returns (bytes32) {
         if (b) {
             return bytes32(uint256(x) + uint256(57896044618658097711785492504343953926634992332820282019728792003956564819968));
             //NOTE: 2 ** 255 = 57896044618658097711785492504343953926634992332820282019728792003956564819968
@@ -153,8 +113,7 @@ contract BGRSmall {
         }
     }
 
-
-    function testsplitreverse() returns (bytes32) {
+    function testsplitinverse() returns (bytes32) {
         //output should be
         //{206, 119, 223, 247, 50, 131, 194, 188, 88, 248, 137, 233, 130, 2, 141, 71, 11, 164, 233, 127, 219, 134, 220, 38, 255, 153, 198, 120, 110, 214, 92, 229}
         bytes32 test = bytes32(35492202744065384896209724461799058330743915199574411635769998350682397498597);
@@ -206,6 +165,105 @@ contract BGRSmall {
         bytes32 x_0 = bytes32(54560730099872202741014348823700252121970690209029593489829040881417946844283);
         bytes32 h_0 = bytes32(30388886992207340305962404453616357726282684957228414817761178193477287654283);
         return h_prev == h_0 && x_prev == x_0;
+    }
+
+    function testIterationWhereBTrue() returns(bool) {
+        //Not working yet
+        /*
+       b: [false true]
+       pk0: [{100559639697891460902764141020356907258428814093396659529555505846242710753001 65537}
+       pk1: {75514132473334443068127286379962420780446675478900207212147158057076204097627 65537}]
+       sigx 5478324901913405590120700960084756849033925727756286198862916110070595692811
+       sigh 31908922008483892227221543185441079020097797844702142216235016446839414122921
+       r0  31713
+       r1 16181
+
+       x0 48067657424336353606332271955203152262108042684260758989777771218257675402919
+       h0 110698550962008875660816179094143079823491013343052579972863588172519883248496
+       */
+
+        bytes32 x_prev = bytes32(5478324901913405590120700960084756849033925727756286198862916110070595692811); //sig.x
+        bytes32 h_prev = bytes32(31908922008483892227221543185441079020097797844702142216235016446839414122921); //sig.h
+
+        bytes32 g;
+        bytes32 eta;
+        bytes32 y;
+        bytes32 X;
+
+        //Line 2
+        X = split_inverse(true, x_prev);
+        y = pi(x_prev, 75514132473334443068127286379962420780446675478900207212147158057076204097627);
+
+        //Line 3 G
+        g = GHash(h_prev);
+
+        //Line 4
+        x_prev = g ^ y;
+
+        //Line 5 H
+        eta = HHash(75514132473334443068127286379962420780446675478900207212147158057076204097627, "MESSAGE 1", 16181, x_prev);
+
+        //Line 6
+        h_prev = h_prev ^ eta;
+
+        //Now should be x1 h1
+        bytes32 x_0 = bytes32(48067657424336353606332271955203152262108042684260758989777771218257675402919);
+        bytes32 h_0 = bytes32(110698550962008875660816179094143079823491013343052579972863588172519883248496);
+        return x_prev == x_0;
+    }
+
+    function testIterationWhereBTrue2() returns(bytes32) {
+        //Not working yet
+        /**
+        b: [false true]
+        pk0: [{103461757571518832622635512335759653289686535967345491211562066594885636499703 65537}
+        pk1: {99730258665501075335213668211394431132735782537502029889654257709728791831553 65537}]
+        x 17345272308542689518344084692542391725387407987955954252876114177875730479097
+        h 72508440660091421077602028370085314092271011290385113018893260330849832526754
+        r0  27913
+        r1 23120
+
+        x0: [16 117 168 232 232 6 14 60 61 167 169 42 196 223 3 180 96 96 224 244 134 86 114 132 48 192 49 62 102 141 90 86]
+        x0: 7444892456440163573410901007413933802788727521996010232230772429884100532822
+        h0 [148 245 112 65 234 115 82 244 229 242 6 16 107 207 73 11 245 140 187 179 248 150 57 138 67 128 252 117 2 51 194 171]
+        h0: 67375953893872021360571726900073981219547307846512650979748574264878368539307
+        */
+
+        bytes32 x_prev = bytes32(17345272308542689518344084692542391725387407987955954252876114177875730479097); //sig.x
+        bytes32 h_prev = bytes32(72508440660091421077602028370085314092271011290385113018893260330849832526754); //sig.h
+
+        bytes32 g;
+        bytes32 eta;
+        bytes32 y;
+        bytes32 X;
+
+        //Line 2
+        X = split_inverse(true, x_prev);
+        y = pi(x_prev, 99730258665501075335213668211394431132735782537502029889654257709728791831553);
+
+        //Line 3 G
+        g = GHash(h_prev);
+
+        //Line 4
+        x_prev = g ^ y;
+
+        //Line 5 H
+        eta = HHash(99730258665501075335213668211394431132735782537502029889654257709728791831553, "MESSAGE 1", 23120, x_prev);
+
+        //Line 6
+        h_prev = h_prev ^ eta;
+
+        //Now should be x1 h1
+        return x_prev;
+    }
+
+    function testsplitpi() returns(bytes32) {
+        bytes32 a = split_inverse(true, 27923161953777573553354981145307315045607106610564892606139996960213013027258);
+        return pi(a, 107533363795656919624871251264905461846385118845249129805258716736864472793907);
+
+        //should be:
+        //x [216 46 187 241 18 138 216 192 50 31 82 230 180 8 62 173 220 7 1 115 32 178 191 228 32 170 158 48 43 241 198 23]
+        //x 97782147384832081648842991865189261321407426024020293440409439033677455672855
     }
 
 }
