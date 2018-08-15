8794a6359da3450e231b629a9d0dab4579b997b1
Bjorn van der Laan <bjorn.vdlaan@gmail.com>
2018-08-15 13:26:15 +0200

    added ecdsa

--- schemes/ecdsa/ecdsa.go
+++ schemes/ecdsa/ecdsa.go
@@ -10,24 +10,25 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/crypto/secp256k1"
 	"github.com/miguelmota/go-solidity-sha3"
+	"io/ioutil"
 )
 
 func main() {
-	key := keygen()
-	message := "TEST"
+	key := KeyGen()
 
-	fmt.Println(hex.EncodeToString(solsha3.SoliditySHA3(solsha3.String(message))))
+	msg := read_file()
+	label := fmt.Sprintf("example.com/api/%d", 1)
+	message := msg + label
 
-	sig, prefixedmsg := sign(message, key)
+	sig, prefixedmsg := Sign(message, key)
 
-	fmt.Println("private key:", hex.EncodeToString(math.PaddedBigBytes(key.D, 32)))
 	fmt.Println("address:", hex.EncodeToString(crypto.PubkeyToAddress(key.PublicKey).Bytes()))
-	fmt.Println("message:", hex.EncodeToString(prefixedmsg))
+	fmt.Println("message:", hex.EncodeToString([]byte(message)))
+	fmt.Println("prefixed message:", hex.EncodeToString(prefixedmsg))
 	fmt.Println("signature:", hex.EncodeToString(sig))
-
 }
 
-func keygen() (*ecdsa.PrivateKey) {
+func KeyGen() (*ecdsa.PrivateKey) {
 	key, err := ecdsa.GenerateKey(crypto.S256(), rand.Reader)
 
 	if err != nil {
@@ -37,7 +38,7 @@ func keygen() (*ecdsa.PrivateKey) {
 	return key
 }
 
-func sign(message string, key *ecdsa.PrivateKey) ([]byte, []byte) {
+func Sign(message string, key *ecdsa.PrivateKey) ([]byte, []byte) {
 	// Turn the message into a 32-byte hash
 	hash := solsha3.SoliditySHA3(solsha3.String(message))
 	// Prefix and then hash to mimic behavior of eth_sign
@@ -49,4 +50,15 @@ func sign(message string, key *ecdsa.PrivateKey) ([]byte, []byte) {
 	}
 
 	return sig, prefixed
+}
+
+func read_file() (string) {
+	b, err := ioutil.ReadFile("data.txt") // just pass the file name
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	//fmt.Println(b) // print the content as 'bytes'
+
+	return string(b) // convert content to a 'string'
 }
